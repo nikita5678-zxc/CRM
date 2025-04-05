c4cb8d12d61f0b9f4e21b9ccd59d1c77a79ea802
nikita5678-zxc <nikita_deybert1111@mail.ru>
2025-04-05 17:01:39 +0300

    подключил БД

---
+++ pkg/postgresSQL/postgres.go
@@ -0,0 +1,36 @@
+package postgresSQL
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	repeatable "gorilla/mux/pkg/postgresSQL/utils"
+	"postgresSQL/utils"
+	"time"
+)
+
+type Ticket interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+func NewTicket(ctx context.Context, maxAttempts int, username, password, host, port, database string) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", username, password, host, port, database)
+	repeatable.DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.Connect(ctx, dsn)
+		if err != nil {
+
+		}
+
+		return nil
+	}, maxAttempts, 5*time.Second)
+
+	return
+}
